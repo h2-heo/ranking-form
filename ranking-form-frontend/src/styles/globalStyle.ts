fbe4f7f73adb225c311eb718c8bb7b42b656afb0
h2-heo <h0h6h2h5@gmail.com>
2020-11-07 11:14:26 +0000

    Apply emotion

---
+++ ranking-form-frontend/src/styles/globalStyle.ts
@@ -0,0 +1,19 @@
+import { css } from '@emotion/core';
+
+const style = css`
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto',
+      'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans',
+      'Helvetica Neue', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+      monospace;
+  }
+`;
+
+export default style;
